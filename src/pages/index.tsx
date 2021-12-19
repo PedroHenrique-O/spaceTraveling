1cf63a2021f7dd4832cc83f3f979bff7521a8312
Pedro <pedrokaz.henrique@gmail.com>
2021-12-19 16:48:32 -0300

    displaying posts from query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { GetStaticProps } from 'next';
 import Prismic from '@prismicio/client';
 
 import Link from 'next/link';
+import { useEffect } from 'react';
 import { getPrismicClient } from '../services/prismic';
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -27,11 +28,12 @@ interface HomeProps {
 }
 
 export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  console.log(postsPagination.results);
   return (
-    <main className={styles.container}>
+    <main className={commonStyles.container}>
       <div className={styles.logoWrapp}> Logo </div>
       {postsPagination.results.map((posts: Post) => (
-        <Link key={posts.uid} href={`/posts/${posts.uid}`}>
+        <Link key={posts.uid} href={`/post/${posts.uid}`}>
           <div className={styles.postWrapp}>
             <h1> {posts.data.title} </h1>
             <p>{posts.data.subtitle}</p>
@@ -55,8 +57,14 @@ export const getStaticProps: GetStaticProps = async () => {
 
   const response = postsResponse.results.map((posts: Post) => {
     return {
-      slug: posts.uid,
-      first_publication_date: posts.first_publication_date,
+      uid: posts.uid,
+      first_publication_date: new Date(
+        posts.first_publication_date
+      ).toLocaleDateString('pt-br', {
+        day: '2-digit',
+        month: 'short',
+        year: 'numeric',
+      }),
       data: {
         title: posts.data.title,
         subtitle: posts.data.subtitle,
