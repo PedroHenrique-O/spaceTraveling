b43f6e7f3a5de964b94afb186d19c6c6c186a6e6
Pedro <pedrokaz.henrique@gmail.com>
2021-12-20 19:37:46 -0300

    added header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,11 @@
-export default function Header() {
-  // TODO
+import Image from 'next/image';
+import styles from './header.module.scss';
+import Logo from '../../../public/Logo.svg';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={styles.container}>
+      <img src="/logo.svg" alt="logo" />
+    </header>
+  );
 }
