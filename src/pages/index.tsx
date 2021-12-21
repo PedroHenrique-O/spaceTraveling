020eeebaf74b996ea1d18e81c5fb158501f65c2c
Pedro <pedrokaz.henrique@gmail.com>
2021-12-20 21:01:38 -0300

    added see more button and its functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
-/* eslint-disable no-console */
 import { GetStaticProps } from 'next';
 import Prismic from '@prismicio/client';
 
 import Link from 'next/link';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { format } from 'date-fns';
+import ptBr from 'date-fns/locale/pt-BR';
 import { getPrismicClient } from '../services/prismic';
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import Header from '../components/Header';
 
 interface Post {
   uid?: string;
@@ -28,11 +30,53 @@ interface HomeProps {
 }
 
 export default function Home({ postsPagination }: HomeProps): JSX.Element {
-  console.log(postsPagination.results);
+  const [post, setPost] = useState<Post[]>(
+    postsPagination.results.map(formatted => ({
+      ...formatted,
+      first_publication_date: format(
+        new Date(formatted.first_publication_date),
+        'dd MM yyyy',
+        { locale: ptBr }
+      ),
+    }))
+  );
+  const [nextPage, setNextPage] = useState(postsPagination.next_page);
+  const [loadMore, setLoadMore] = useState<Post[]>([]);
+
+  useEffect(() => {
+    async function getNextPage(): Promise<void> {
+      const response = await fetch(`${nextPage}`);
+      const data = await response.json();
+      setLoadMore(data.results);
+    }
+    getNextPage();
+  }, []);
+
+  const handleLoadMorePosts = (): void => {
+    const newState = [...post];
+
+    const newLoadMore = loadMore.map((loadMore1: Post) => ({
+      uid: loadMore1.uid,
+      first_publication_date: format(
+        new Date(loadMore1.first_publication_date),
+        'dd MM yyyy',
+        { locale: ptBr }
+      ),
+      data: {
+        title: loadMore1.data.title,
+        author: loadMore1.data.author,
+        subtitle: loadMore1.data.subtitle,
+      },
+    }));
+
+    newState.push(...newLoadMore);
+    setPost(newState);
+  };
+
   return (
     <main className={commonStyles.container}>
-      <div className={styles.logoWrapp}> Logo </div>
-      {postsPagination.results.map((posts: Post) => (
+      <Header />
+      {post.map(posts => (
         <Link key={posts.uid} href={`/post/${posts.uid}`}>
           <div className={styles.postWrapp}>
             <h1> {posts.data.title} </h1>
@@ -42,6 +86,15 @@ export default function Home({ postsPagination }: HomeProps): JSX.Element {
           </div>
         </Link>
       ))}
+      {!!nextPage && (
+        <button
+          onClick={handleLoadMorePosts}
+          className={styles.loadMorePosts}
+          type="submit"
+        >
+          Carregar mais
+        </button>
+      )}
     </main>
   );
 }
@@ -51,20 +104,14 @@ export const getStaticProps: GetStaticProps = async () => {
   const postsResponse = await prismic.query(
     [Prismic.predicates.at('document.type', 'nextblog1')],
     {
-      pageSize: 2,
+      pageSize: 1,
     }
   );
 
   const response = postsResponse.results.map((posts: Post) => {
     return {
       uid: posts.uid,
-      first_publication_date: new Date(
-        posts.first_publication_date
-      ).toLocaleDateString('pt-br', {
-        day: '2-digit',
-        month: 'short',
-        year: 'numeric',
-      }),
+      first_publication_date: posts.first_publication_date,
       data: {
         title: posts.data.title,
         subtitle: posts.data.subtitle,
@@ -78,9 +125,8 @@ export const getStaticProps: GetStaticProps = async () => {
     next_page: postsResponse.next_page,
   };
 
-  console.log('---------', postsPagination.results, '---------');
-
   return {
     props: { postsPagination },
+    revalidate: 60 * 60,
   };
 };
