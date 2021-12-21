805e5b8aa6575678d2e4628114695ed1abff10ab
Pedro <pedrokaz.henrique@gmail.com>
2021-12-20 21:01:50 -0300

    added next domain

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// next.config.js
+module.exports = {
+  images: {
+    domains: ['https://images.prismic.io', 'images.prismic.io'],
+  },
+};
