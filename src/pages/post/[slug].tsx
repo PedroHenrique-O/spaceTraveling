7b138237423ed8d1444ff59ba000e93f40c2d109
Pedro <pedrokaz.henrique@gmail.com>
2021-12-22 16:46:32 -0300

    added estimatedtime and staticPath

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,15 +1,20 @@
-/* eslint-disable react/no-danger */
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react/self-closing-comp */
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { RichText } from 'prismic-dom';
 
+import { useMemo } from 'react';
 import Prismic from '@prismicio/client';
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
 import Head from 'next/head';
 import Image from 'next/image';
+import {
+  AiOutlineClockCircle,
+  AiOutlineUser,
+  AiOutlineCalendar,
+} from 'react-icons/ai';
+import { useRouter } from 'next/router';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
@@ -20,7 +25,7 @@ interface Post {
   first_publication_date: string | null;
   data: {
     title: string;
-    image: {
+    banner: {
       url: string;
     };
     author: string;
@@ -38,6 +43,33 @@ interface PostProps {
 }
 
 export default function Post({ post }: PostProps): JSX.Element {
+  const router = useRouter();
+
+  const formattedDate = format(
+    new Date(post.first_publication_date),
+    'dd MMM yyyy',
+    {
+      locale: ptBR,
+    }
+  );
+
+  const EstimatedReadTime = useMemo(() => {
+    let fullText = '';
+
+    const wordsPerMinute = 200;
+
+    post?.data.content.forEach(postContent => {
+      fullText += postContent.heading;
+      fullText += RichText.asText(postContent.body);
+    });
+    const time = Math.ceil(fullText.split(/\s+/).length / wordsPerMinute);
+    return time;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (router.isFallback) {
+    return <h1>Carregando...</h1>;
+  }
   return (
     <>
       <Header />
@@ -45,73 +77,87 @@ export default function Post({ post }: PostProps): JSX.Element {
       <div className={styles.bannerWrap}>
         <Image
           alt="banner"
-          width="1440px"
+          width="1400px"
           height="350px"
-          src={post.data.image.url}
+          src={post.data.banner.url}
         />
       </div>
       <main className={commonStyles.container}>
         <div className={styles.titleWrapp}>
           <h1>{post.data.title}</h1>
           <time>
-            {new Date(post.first_publication_date).toLocaleDateString('pt-BR', {
-              day: '2-digit',
-              month: 'short',
-              year: 'numeric',
-            })}
+            <AiOutlineCalendar />
+            {`${formattedDate}`}
           </time>
-          <span> {post.data.author} </span>
-          <span className={styles.time}> 4m </span>
+
+          <span>
+            <AiOutlineUser />
+            {post.data.author}
+          </span>
+          <span id="time" className={styles.time}>
+            <AiOutlineClockCircle />
+            {EstimatedReadTime} min
+          </span>
         </div>
 
-        <section className={styles.contentWrapper}>
-          {post.data.content.map((content, index) => (
-            <>
-              <h1> {RichText.asText(content.heading)} </h1>
+        {post.data.content.map(content => {
+          return (
+            <section key={content.heading} className={styles.contentWrapper}>
+              <h2>{content.heading} </h2>
+
               <div
+                // eslint-disable-next-line react/no-danger
                 dangerouslySetInnerHTML={{
                   __html: RichText.asHtml(content.body),
                 }}
-              />
-            </>
-          ))}
-        </section>
+              ></div>
+            </section>
+          );
+        })}
       </main>
     </>
   );
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient();
+  const posts = await prismic.query(
+    [Prismic.predicates.at('document.type', 'nextblog1')],
+    { pageSize: 1 }
+  );
+  const paths = posts.results.map(post => ({
+    params: { slug: post.uid },
+  }));
   return {
-    paths: [],
-    fallback: 'blocking',
+    paths,
+    fallback: true,
   };
-  // const prismic = getPrismicClient();
-  // const posts = await prismic.query([
-  //   Prismic.predicates.at('document.type', 'nextblog1'),
-  // ]);
-
-  // const paths = posts.results;
-
-  // return {
-  //   paths: posts.results.map(post => {
-  //     return { params: { slug: post.uid } };
-  //   }),
-  //   fallback: 'blocking',
-  // };
 };
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const { slug } = params;
+export const getStaticProps: GetStaticProps = async context => {
+  const { slug } = context.params;
 
   const prismic = getPrismicClient();
   const response: Post = await prismic.getByUID('nextblog1', String(slug), {});
 
-  const post = response;
-  console.log('banner: ', JSON.stringify(post.data.content, null, 3));
+  const post: Post = {
+    first_publication_date: response.first_publication_date,
+    data: {
+      title: response.data.title,
+      author: response.data.author,
+      banner: {
+        url: response.data.banner.url,
+      },
+      content: response.data.content.map(content => ({
+        heading: content.heading,
+        body: [...content.body],
+      })),
+    },
+  };
 
   return {
     props: { post },
-    revalidate: 1,
+
+    redirect: 60 * 1, // one minute
   };
 };
